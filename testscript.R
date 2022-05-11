74af239d1be81069641fa1b60e4b4e2dca816978
Marie Ternes <59573980+MarieTernes@users.noreply.github.com>
2022-05-11 11:29:43 +0200

    Update testscript.R

--- testscript.R
+++ testscript.R
@@ -14,56 +14,49 @@ library(spcov)
 library(Rcpp)
 library(RcppArmadillo)
 library(RcppEigen)
+library(corrplot)
+library(igraph)
 
 # Source functions
-source("sparseMFVAR_Functions.R")
-source("inputs_Functions.R")
-source("coincident_indicator.R")
-sourceCpp("FunctionsRcpp.cpp")
+source("sparseMFVAR_Functions_R1.R")
+source("inputs_function_R1.R")
+source("coincident_indicator_R1.R")
+sourceCpp("FunctionsRcpp_R1.cpp")
 
 #########################################################################
 #-------------- Data example & Data pre-processing ---------------------#
 #########################################################################
-load("example_quarterly.RData")
-load("example_monthly.RData")
-data = make_data_matrices(data_quarterly = example_quarterly, data_monthly = example_monthly)
-Y_VAR = data$Y_VAR
+load("mfvar_small.RData")
+data = mfvar_small
+Y_VAR = data$Y_VAR[1:125,]
 k_q = data$k_q
 k_m = data$k_m
 k_w = data$k_w
 k_d = data$k_d
 K = data$K
+p = 1
 
 #############################################################################
 #-------------- Obtaining the hierarchical regularizer ---------------------#
 #############################################################################
-MFHIER <- sparseMFVAR(Y_VAR = Y_VAR, k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, 
-                      penalty_H_on_L = "HIER", penalty_L_on_H = "HIER", penalty_own_on_own = "HIER",
-                      inputsProx = NULL,l_lambda_beta = 10, standardize = TRUE, epsilon = 1e-3, 
+inputsProx = inputs_sparseMFVAR(k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, p = p,
+                                penalty_H_on_L = "HIER", penalty_L_on_H = "HIER", penalty_own_on_own = "HIER")
+
+# If inputsProx = NULL (default) it is calculated within the function sparseMFVAR
+# However, for out-of-sample exercises since the object inputsProx does not change across the samples, it is possible to increase the speed of the code and give inputsProx as an input to MFHIER.
+MFHIER <- sparseMFVAR(Y_VAR = Y_VAR, k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, p = p, 
+                      penalty_H_on_L = "HIER", penalty_L_on_H = "HIER", penalty_own_on_own = "HIER", 
+                      inputsProx = inputsProx, l_lambda_beta = 10, standardize = TRUE, epsilon = 1e-3, 
                       max_iter = 400)
-MFHIER$betas  
 
 # Get optimal tuning parameter via time series cross validation 
 # rolling window and one-step-ahead MSFE as a cross-validation score
-CV <- MFVAR_cv(Y_VAR = Y_VAR, k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, 
+CV <- MFVAR_cv(Y_VAR = Y_VAR, k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, p = p,
                penalty_H_on_L = "HIER", penalty_L_on_H = "HIER", penalty_own_on_own = "HIER",
+               inputsProx = inputsProx,
                lambdas_beta = MFHIER$lambdas_beta, cvcut = 20, standardize = TRUE,
                epsilon = 1e-3, max_iter = 400)
-CV$lambda_opt
-CV$lambda_opt_oneSE
-which(MFHIER$lambdas_beta == CV$lambda_opt)
-which(MFHIER$lambdas_beta == CV$lambda_opt_oneSE)
-
-#######################################################################################################
-#------------------------------------- out-of-sample-exercise ----------------------------------------#
-# To perform a "pseudo" out-of-sample exercise it is possible to use MFHIER as explained above. 
-# If inputsProx = NULL (default) it is calculated within the function sparseMFVAR
-# However, since the object inputsProx does not change across the samples, it is possible to increase
-# the speed of the code and give inputsProx as an input to MFHIER.
-# The object inputsProx can be calculated using the function inputs_sparseMFVAR
-#######################################################################################################
-inputsProx = inputs_sparseMFVAR(k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, 
-                                penalty_H_on_L = "HIER", penalty_L_on_H = "HIER", penalty_own_on_own = "HIER")
+cv_opt = which(MFHIER$lambdas_beta == CV$lambda_opt)
 
 
 #################################################################################################
@@ -71,17 +64,16 @@ inputsProx = inputs_sparseMFVAR(k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d,
 #------------------------ for a variable in the Y_VAR time series matrix -----------------------#
 #################################################################################################
 ind = coincident_indicator(Y_VAR = Y_VAR, betas = MFHIER$betas, 
-                          k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d,  
-                          l_lambda_sigma = 10, col_coinci = 1)
-ind$maxcor
-ind$coincInd
-ind$series_coincInd
+                           k_q = k_q, k_m = k_m, k_w = k_w, k_d = k_d, p = p, 
+                           l_lambda_sigma = 10, col_coinci = 1)
 
-# Plot standardized variables vs standardized coincident indicator
-plot(ind$y, type ='l', col ='black', ylab = "", lwd = 1.7)
-lines(ind$coincInd, col = 'blue', lwd = 1.7)
-legend("bottomleft", bty = "n", legend = c("GDP growth", "Coincident indicator"), lty = c(1),col = c("black", "blue"))
+# Plot variables vs standardized coincident indicator
+GDP = ts(ind$y, start = c(1987,4), end = c(2018,4), frequency = 4)
+indicator = ts(ind$coincInd, start = c(1987,4), end = c(2018,4), frequency = 4)
 
-
+# Small
+plot(GDP, type ='l', main ="Coincident indicator from small MF-VAR", col ='black', ylab = "", lwd = 1.7)
+lines(indicator, col = c('#2b8cbe'), lwd = 1.9)
+legend("bottomleft", bty = "n", legend = c("GDP growth", "Coincident indicator"), lty = c(1), lwd = c(1.7,1.7), col = c("black", '#2b8cbe'))
 
   
